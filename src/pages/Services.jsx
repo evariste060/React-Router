69a231a659f1acc5011843fe510dca6289762ae9
Evariste Nkurunziza <nkurunzizaevariste060@gmail.com>
2026-04-07 16:33:34 +0200

    Add the counter that use useReduce hook to work

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,5 +1,24 @@
-export default function Services(){
-    return (
-        <h1 className="text-2xl font-semibold text-blue-500 text-center">Welcome to Services Page</h1>
-    )
+import { useState, useReducer } from "react";
+function reducer(state,action) {
+    switch(action.type){
+        case "increment":
+           return  {count: state.count +1}
+        case "Decrement":
+           return  {count : state.count -1}
+        default:
+            return state
+    }
+    
 }
+function Services() {
+    const [state,dispatch] = useReducer(reducer,{count:0})
+    return (
+        <div className="flex flex-col items-center">
+            <h1 className="font-bold text-4xl">{state.count}</h1>
+            <button  className="w-40 bg-blue-500  font-bold text-2xl rounded-2xl"  onClick={()=>dispatch({type : "increment"})}>+</button>
+            <button className="w-40 bg-blue-500  font-bold text-2xl  rounded-2xl" onClick={() =>dispatch({type : "Decrement"})}>-</button>
+        </div>
+    )
+ }
+
+export default Services; 
